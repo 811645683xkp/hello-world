220cc0d7a741b1d50153092546555ea6b2f5504c
XiaoFangYuan <811645683@qq.com>
2024-07-18 21:44:06 +0800

    修改HelloDemmo ndrc_0.1.01

--- src/main/java/com/HelloDemmo.java
+++ src/main/java/com/HelloDemmo.java
@@ -3,6 +3,7 @@ package src.main.java.com;
 public class HelloDemmo {
 
     public static void main(String[] args) {
+        System.out.println("修改内容---------");
         System.out.println("hello world");
         System.out.println("==================");
     }
